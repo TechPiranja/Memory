ece2e8e9b03a3b9319241f74480eb8496969acf0
Piranja <techpiranja@gmail.com>
2020-06-04 17:49:36 +0200

    + added favicon and basic structure

---
+++ script.js
@@ -0,0 +1,5 @@
+function setPairRange() {
+  var val = document.getElementById("pairRange").value; //gets the oninput value
+  document.getElementById("output").innerHTML = val; //displays this value to the html page
+  console.log(val);
+}
